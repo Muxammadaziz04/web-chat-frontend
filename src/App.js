16abfa0e476c6876864d7cf781b99b54c5878715
Muxammadaziz04 <muxammadazizramziddinov@gmail.com>
2022-08-07 23:02:50 +0500

    user online va qachon online boganligi qoshildi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import Layout from './components/Layout'
 import DefaultMessageComponent from './components/Messages/DefaultMessageComponent'
 import Messages from './components/Messages'
-import {store} from './redux/store'
+import { store } from './redux/store'
+import { host, token } from './constants'
 
 function App() {
+
+  useEffect(() => {
+    const options = {
+      method: 'PUT',
+      headers: {
+        token
+      }
+    }
+
+    const setOnlineFunc = async () => {
+      await fetch(`${host}/online`, options)
+    }
+    setOnlineFunc()
+    
+    window.addEventListener('beforeunload', async () => {
+      await fetch(`${host}/leave`, options)
+    })
+  }, [])
+
   return (
     <BrowserRouter>
       <Provider store={store}>
